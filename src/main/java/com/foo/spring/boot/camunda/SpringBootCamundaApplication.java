b6d0500161da7b07abb5de416ffec4c0a37cfc6f
simoganger <simonsoft7@gmail.com>
2021-04-22 01:50:19 +0100

    First commit

---
+++ src/main/java/com/foo/spring/boot/camunda/SpringBootCamundaApplication.java
@@ -0,0 +1,13 @@
+package com.foo.spring.boot.camunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCamundaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCamundaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foo/spring/boot/camunda/SpringBootCamundaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foo.spring.boot.camunda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCamundaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
